97208a7bad327bd72050659947faa99d4e266e2e
lloydik <54674037+lloydik@users.noreply.github.com>
2020-05-23 10:19:41 +0300

    Игра
    теперь до неё можно дойти

--- learn/script.js
+++ learn/script.js
@@ -7,6 +7,8 @@
             ['<img src="../img/js-logo%20(1).png">'],
             ['Введение. addEventListener. innerHTML'],
             ['classList. Условный оператор'],
+            [''],
+            ['К секретному уровню'],
         ];
 
         let mainEn = [
@@ -15,12 +17,14 @@
             ['<img src="../img/js-logo%20(1).png">'],
             ['Introduction. addEventListener. innerHTML'],
             ['classList. condition operator'],
+            [''],
+            ['To secret level'],
         ];
         let menuEn= ['Learn', '<img class="home" src="../img/JS-logo.png">','About', ''];
         let menuRu= ['Изучить', '<img class="home" src="../img/JS-logo.png">','О проекте', ''];
 
-let footerRu= ['Глазырин Артём, Кукин Максим', '<a href = "https://informatics.ru/"><img src ="../../img/Logo.svg"></a>'];
-let footerEn= ['Glazyrin Artem, Kukin Maxim', '<a href = "https://informatics.ru/"><img src ="../../img/Logo.svg"></a>'];
+let footerRu= ['Глазырин Артём, Кукин Максим', '<a href = "https://informatics.ru/"><img src ="../img/Logo.svg"></a>'];
+let footerEn= ['Glazyrin Artem, Kukin Maxim', '<a href = "https://informatics.ru/"><img src ="../img/Logo.svg"></a>'];
         let olRu = [];
         let olEn = [];
         
@@ -62,3 +66,21 @@ let footerEn= ['Glazyrin Artem, Kukin Maxim', '<a href = "https://informatics.ru
         switchLanguage.addEventListener('click', function() {
             setContent();
         });
+
+
+
+
+document.querySelector('main > button').addEventListener('click', function(){
+    if (document.body.querySelector('main').lastElementChild.tagName == 'A')
+        document.body.querySelector('main').removeChild(document.body.querySelector('main').lastElementChild);
+    let a = document.createElement('A');
+    if (secretField.value == 'flag{JS_i5_C0o1}')
+        {
+            a.innerHTML = 'g0!';
+            a.setAttribute('href', 'game/level1/index.html');
+        }
+        
+    else
+        a.innerHTML = 'no';
+    document.body.querySelector('main').appendChild(a);
+});
