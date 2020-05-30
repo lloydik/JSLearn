5efc3dfb7f3442428a78bb384c7848c5971a1739
lloydik <54674037+lloydik@users.noreply.github.com>
2020-05-30 11:24:45 +0300

    пофиксил баг со сменой языков в разделе learn

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+        let headerRu = ['&nbsp;'];
+        let headerEn = ['&nbsp;'];
+        let mainRu = [
+            ['Добро Пожаловать!'],
+            ['Давай выберем тему для изучения'],
+            ['<img src="../img/js-logo%20(1).png">'],
+            ['Введение. addEventListener. innerHTML'],
+            ['classList. Условный оператор'],
+            [`<h3>Попасть на уровень</h3>
+            <div class="form-row">
+                <input type="text" id="pass" required autocomplete="off"><label for="pass">Флаг</label>
+            </div>
+            <p><input type="button" value="Войти" id="ent"></p>`],
+        ];
+
+        let mainEn = [
+            ['Welcome!'],
+            ['Let\'s choose your theme for learning!'],
+            ['<img src="../img/js-logo%20(1).png">'],
+            ['Introduction. addEventListener. innerHTML'],
+            ['classList. condition operator'],
+            [`<h3>Get to level</h3>
+            <div class="form-row">
+                <input type="text" id="pass" required autocomplete="off"><label for="pass">Flag</label>
+            </div>
+            <p><input type="button" value="Enter" id="ent"></p>`],
+        ];
+        let menuEn= ['Learn', '<img class="home" src="../img/JS-logo.png">','About', ''];
+        let menuRu= ['Изучить', '<img class="home" src="../img/JS-logo.png">','О проекте', ''];
+
+let footerRu= ['Глазырин Артём, Кукин Максим', '<a href = "https://informatics.ru/"><img src ="../img/Logo.svg"></a>'];
+let footerEn= ['Glazyrin Artem, Kukin Maxim', '<a href = "https://informatics.ru/"><img src ="../img/Logo.svg"></a>'];
+        let olRu = [];
+        let olEn = [];
+        
+        
+        
+        let RuPage = [];
+        RuPage.push(headerRu);
+        RuPage.push(menuRu);
+        RuPage.push(mainRu);
+        RuPage.push(footerRu);
+        let EnPage = [];
+        EnPage.push(headerEn);
+        EnPage.push(menuEn);
+        EnPage.push(mainEn);
+        EnPage.push(footerEn);
+        let en = true;
+        function setContent() {
+            let array = document.body.children;
+            let count1 = array.length;
+            if (en) {
+                for (let i = 1; i < count1; i++) {
+                    let count2 = array[i].childElementCount;
+                    for (let j = 0; j < count2; j++) {
+                        array[i].children[j].innerHTML = RuPage[i][j];
+                    }
+                }
+                switchLanguage.setAttribute('src', '../img/SLEnglish.jpg');
+            } else {
+                for (let i = 1; i < count1; i++) {
+                    let count2 = array[i].childElementCount;
+                    for (let j = 0; j < count2; j++) {
+                        array[i].children[j].innerHTML = EnPage[i][j];
+                    }
+                }
+                switchLanguage.setAttribute('src', '../img/SLRussia.png');
+            }
+            en = !en;
+        }
+        switchLanguage.addEventListener('click', function() {
+            setContent();
+        });
+
+
+
+
+document.querySelector('#ent').addEventListener('click', function(){
+    if (document.body.querySelector('main').lastElementChild.tagName == 'A')
+        document.body.querySelector('main').removeChild(document.body.querySelector('main').lastElementChild);
+    let a = document.createElement('A');
+    if (pass.value == 'flag{JS_i5_C0o1}')
+        {
+            a.innerHTML = 'g0!';
+            a.setAttribute('href', 'game/level1/index.html');
+        }
+        
+    else
+        a.innerHTML = 'no';
+    document.body.querySelector('main').appendChild(a);
+});
